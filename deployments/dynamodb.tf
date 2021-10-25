859ec55c249cbf47ac85772e5d43d1b6d44b02cd
Zach <zach.nicoll55@gmail.com>
2021-10-25 22:21:16 +1000

    fix(deployments): change filters attribute to N instead of L

--- deployments/dynamodb.tf
+++ deployments/dynamodb.tf
@@ -17,7 +17,26 @@ resource "aws_dynamodb_table" "ddbtable" {
 
   attribute {
     name = "date_created"
-    type = "S"
+
+    // UNIX timestamp (seconds)
+    type = "N"
+  }
+
+  attribute {
+    name = "filters"
+
+    // Will be a list of filter enumerations
+    type = "N"
+  }
+
+  # Make the filters array an index, so we can query by filter
+  global_secondary_index {
+    name            = "ImageFiltersIndex"
+    hash_key        = "filters"
+    range_key       = "date_created"
+    read_capacity   = 20
+    write_capacity  = 20
+    projection_type = "ALL"
   }
 
   lifecycle {
