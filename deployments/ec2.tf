c92a8113f7be7319581a78cb3ca5c87aaeac1c9f
Zach <zach.nicoll55@gmail.com>
2021-10-28 13:32:13 +1000

    feat(deployments): create ec2 instance, and an ami based on it

---
+++ deployments/ec2.tf
@@ -0,0 +1,32 @@
+variable "processor_ami_name" {
+  default = "image_processor_ami"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "image_processor" {
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = "t2.micro"
+  security_groups = ["CAB432SG"]
+  subnet_id       = var.subnet_id
+}
+
+resource "aws_ami_from_instance" "image_processor_ami" {
+  name               = var.processor_ami_name
+  source_instance_id = aws_instance.image_processor.id
+}
+
