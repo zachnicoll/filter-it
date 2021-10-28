6414032c6efbb9c25684edaf1440b74cd07a3a0c
Zach <zach.nicoll55@gmail.com>
2021-10-28 15:02:18 +1000

    feat(filterit): more todos in image processor, build dockerfile

--- internal/pkg/filterit/image_processor.go
+++ internal/pkg/filterit/image_processor.go
@@ -1,5 +1,7 @@
 package filterit
 
+import "fmt"
+
 func applyFilters(filters []int) {
 	// TODO: Use image magik to apply each of the filters to the image
 }
@@ -18,12 +20,18 @@ func processMessage(id string) {
 	// TODO: Write new filenname to document's image attribute
 
 	// TODO: Set document progress attribute to DONE
+
+	// TODO: Invalid cache with all keys containing filters (use KEYS command)
 }
 
 func WatchQueue() {
+	for {
+		fmt.Print("WATCHING QUEUE")
+	}
 	// TODO: In a loop, check if the SQS queue has a new message
 
 	// TODO: If message, spin off a subroutine and process the message - processMessage(id)
 
 	// TODO: Log custom metric to CloudWatch
+
 }
