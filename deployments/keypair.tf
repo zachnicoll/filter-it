30ab3d2b4e9f97ad6331d70d793f91b781d26a36
Audi Bailey <perdana.bailey@connect.qut.edu.au>
2021-10-31 00:38:26 +1000

    fix(deployments): ec2 ami terraform works

--- deployments/keypair.tf
+++ deployments/keypair.tf
@@ -2,11 +2,6 @@ variable "key_name" {
   default = "filterit-terraform"
 }
 
-# Make sure to export TF_VAR_PEM_PATH 
-variable "PEM_PATH" {
-  type = string
-}
-
 resource "aws_key_pair" "image_processor_key" {
   key_name   = var.key_name
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCSB/ZHcGkC5bZNb2EcNqyDOk7HnzbGSWhBcP/MuQuqwr+Dt+rUy9cPIooFAUhI2ixFDiEFayt1Ias3N5BEqpMgYdk3e2mpelmr9W06ojpcNdiw5Oxtf9zUs2xV9ieXJfRGS+u/ILsAX+DyW5inxNCk3u2fg87PBoYfpRJ1cyxv7Uyi2/S2v4Qq9u19YFU75NunRovpfheYSOSVh3it3u3Vc+QHRnl6ziRH3cn0zrPZ8PLt2wDLsH4DS+jLSczKAA+ZhaQq4JIbNFQfixB3sWoRJ8Z4mjciFVIO9EX/IIF84WImNhSN0QfaxwNnmsIR7da7zCrFQIhu9JLP8WFez7zn"
