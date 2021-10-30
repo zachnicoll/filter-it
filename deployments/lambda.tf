129e62897b5809caf029252b46366e15bb485f36
Zach <zach.nicoll55@gmail.com>
2021-10-30 17:21:50 +1000

    fix(deployments): uncomment source code hash for lambdas

--- deployments/lambda.tf
+++ deployments/lambda.tf
@@ -1,11 +1,11 @@
 /* Lambada Upload */
 resource "aws_lambda_function" "lambda_upload" {
-  filename      = data.archive_file.lambda_upload_zip.output_path
-  handler       = "lambda_upload"
-  role          = aws_iam_role.lambda_exec_role.arn
-  runtime       = "go1.x"
-  function_name = "filterit-lambda_upload"
-  # source_code_hash = data.archive_file.lambda_upload_zip.output_base64sha256
+  filename         = data.archive_file.lambda_upload_zip.output_path
+  handler          = "lambda_upload"
+  role             = aws_iam_role.lambda_exec_role.arn
+  runtime          = "go1.x"
+  function_name    = "filterit-lambda_upload"
+  source_code_hash = data.archive_file.lambda_upload_zip.output_base64sha256
 
   vpc_config {
     subnet_ids         = [aws_subnet.filterit-subnet-private-1.id]
@@ -21,17 +21,17 @@ resource "aws_lambda_function" "lambda_upload" {
 
 /* Lambda Queue */
 resource "aws_lambda_function" "lambda_queue" {
-  filename      = data.archive_file.lambda_queue_zip.output_path
-  handler       = "lambda_queue"
-  role          = aws_iam_role.lambda_exec_role.arn
-  runtime       = "go1.x"
-  function_name = "filterit-lambda_queue"
-  # source_code_hash = data.archive_file.lambda_queue_zip.output_base64sha256
+  filename         = data.archive_file.lambda_queue_zip.output_path
+  handler          = "lambda_queue"
+  role             = aws_iam_role.lambda_exec_role.arn
+  runtime          = "go1.x"
+  function_name    = "filterit-lambda_queue"
+  source_code_hash = data.archive_file.lambda_queue_zip.output_base64sha256
 
   environment {
     variables = {
-      AWS_IMAGE_TABLE = var.dynamodb_name
-      AWS_SQS_QUEUE   = var.sqs_name
+      AWS_IMAGE_TABLE   = var.dynamodb_name
+      AWS_SQS_QUEUE     = var.sqs_name
       AWS_REDIS_ADDRESS = element(aws_elasticache_cluster.redis.cache_nodes, 0).address
     }
   }
