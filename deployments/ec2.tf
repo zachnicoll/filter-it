f0e9078961f2c2f6cf3838614388170d7619d8ca
Audi Bailey <perdana.bailey@connect.qut.edu.au>
2021-10-31 16:17:39 +1000

    fix(deployments): attempt to pass through env-vars

--- deployments/ec2.tf
+++ deployments/ec2.tf
@@ -24,8 +24,19 @@ data "aws_ami" "ubuntu" {
    security_groups = [aws_security_group.filterit-sg.id]
    subnet_id       = aws_subnet.filterit-subnet-public-1.id
    key_name        = aws_key_pair.image_processor_key.key_name
+   user_data = templatefile("deploy.sh", {
+       AWS_TABLE = var.dynamodb_name
+       AWS_SQS = var.sqs_name
+       AWS_REDIS = element(aws_elasticache_cluster.redis.cache_nodes, 0).address
+       AWS_S3_BUCKET = var.image_bucket
+     })
 
-   user_data = file("deploy.sh")
+
+   depends_on = [
+     aws_dynamodb_table.ddbtable,
+     aws_sqs_queue.filterit-queue,
+     aws_elasticache_cluster.redis,
+   ]
  }
 
  resource "aws_ami_from_instance" "image_processor_ami" {
