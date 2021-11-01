247cb3a81f4d21811f07f266a964807510e41611
Zach <zach.nicoll55@gmail.com>
2021-11-01 11:33:57 +1000

    fix(deployments): do not export AWS env vars in ec2

--- deployments/deploy.sh
+++ deployments/deploy.sh
@@ -20,15 +20,6 @@ sudo usermod -a -G docker ubuntu
 
 docker pull znicoll/filter-it-image-processor
 
-sudo apt-get install jq -y
-
-TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
-META_DATA=`curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/iam/security-credentials/ec2_exec_role`
-
-export AWS_ACCESS_KEY_ID=$(jq ".AccessKeyId" <<< $META_DATA)
-export AWS_SECRET_ACCESS_KEY=$(jq ".SecretAccessKey" <<< $META_DATA)
-export AWS_SESSION_TOKEN=$(jq ".Token" <<< $META_DATA)
-
 sudo touch /etc/systemd/system/docker.filterit.service
 echo "[Unit]
 Description=FilterIt Image Processor Container
@@ -44,9 +35,6 @@ ExecStart=/usr/bin/docker run \
   -e AWS_SQS_QUEUE=${AWS_SQS} \
   -e AWS_REDIS_ADDRESS=${AWS_REDIS} \
   -e AWS_REGION=${AWS_REGION} \
-  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-  -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-  -e AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN \
   znicoll/filter-it-image-processor
 
 [Install]
